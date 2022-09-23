254406ba3628f518a5132258c235e265a3c5b81f
BezTTTT <Best_1614@hotmail.com>
2022-09-23 14:17:45 +0700

    completed

--- pages/api/room/[roomId]/message.js
+++ pages/api/room/[roomId]/message.js
@@ -4,13 +4,30 @@ import { v4 as uuidv4 } from "uuid";
 export default function roomIdMessageRoute(req, res) {
   if (req.method === "GET") {
     const rooms = readDB();
+    const roomId = req.query.roomId;
+    const findRoom = rooms.findIndex((x) => x.roomId === roomId);
+    if (findRoom === -1)
+      return res.status(404).json({ ok: false, message: "Invalid room id" });
+    return res.json({ ok: true, messages: rooms[findRoom].messages });
   } else if (req.method === "POST") {
     const rooms = readDB();
-
     //read request body
     const text = req.body.text;
-
     //create new id
     const newId = uuidv4();
+    const roomId = req.query.roomId;
+    const findRoom = rooms.findIndex((x) => x.roomId === roomId);
+
+    if (typeof text !== "string")
+      return res.status(400).json({ ok: false, message: "Invalid text input" });
+    if (findRoom === -1)
+      return res.status(404).json({ ok: false, message: "Invalid room id " });
+    const newText = {
+      messageId: newId,
+      text: text,
+    };
+    rooms[findRoom].messages.push(newText);
+    writeDB(rooms);
+    return res.json({ ok: true, message: newText });
   }
 }
--- pages/api/room/[roomId]/message/[messageId].js
+++ pages/api/room/[roomId]/message/[messageId].js
@@ -4,4 +4,18 @@ export default function roomIdMessageIdRoute(req, res) {
   //read value from URL
   const roomId = req.query.roomId;
   const messageId = req.query.messageId;
+  if (req.method === "DELETE") {
+    const rooms = readDB();
+    const findRoom = rooms.findIndex((x) => x.roomId === roomId);
+    if (findRoom === -1)
+      return res.status(404).json({ ok: false, message: "Invalid room id" });
+    const delMsg = rooms[findRoom].messages.findIndex(
+      (x) => x.messageId === messageId
+    );
+    if (delMsg === -1)
+      return res.status(404).json({ ok: false, message: "Invalid message id" });
+    rooms[findRoom].messages.splice(delMsg, 1);
+    writeDB(rooms);
+    return res.json({ ok: true });
+  }
 }
